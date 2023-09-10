83a5adeb29abbfed78d5729e2ddd2c3bd1d74ff1
CCRss <sluminat2@gmail.com>
2023-09-10 17:06:11 +0600

    add new files

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Load profile section
+    const profileSection = document.getElementById("profile-section");
+    profileSection.innerHTML = "<h2>Profile</h2><p>This is my GitHub profile.</p>";
+
+    // Load projects section
+    const projectsSection = document.getElementById("projects-section");
+    projectsSection.innerHTML = "<h2>Projects</h2><p>Project 1: Description</p><p>Project 2: Description</p>";
+});
